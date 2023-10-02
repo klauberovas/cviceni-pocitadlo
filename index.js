b9efea3ac81e16c1f1432b2dec707857a09fd648
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-10-02 20:51:59 +0200

    Done

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
+// fce která vybere elm s číslem a prodlouží ho na dva znaky přidáním O
 const updateCounter = (number) => {
-  const valueElm = document.querySelector('counter__value');
-  valueElm.value = String(numbr).padStart(2, '0');
+  const valueElm = document.querySelector('.counter__value');
+  valueElm.textContent = String(number).padStart(2, '0');
 };
 
 let value = 0;
-const btnUp = document.querySelector('counter__up');
-const btnDown = document.querySelector('counter__down');
+const btnUp = document.querySelector('.counter__up');
+const btnDown = document.querySelector('.counter__down');
 
+// Přičítá se k value a volá se fce updateCounter s novou hodnotou value
 btnUp.addEventListener('click', () => {
-  if (velue < 99) {
-    velue + 1;
-    updateCountr(value);
+  if (value < 99) {
+    value += 1;
+    updateCounter(value);
   }
 });
 
+//Když mačkám down odečítá se od value a volá se fce updateCounter  s novou hodnotou value
 btnDown.addEventListener('click', () => {
-  if (velue > 0) {
-    velue - 1;
-    updateCountr(value);
+  if (value > 0) {
+    value -= 1;
+    updateCounter(value);
   }
 });
-
+//začneme od čísla v let value = 0, kdyby se nevolala začneme od čísla 15 zadaného v HTML a nejde mačkat dolů, protože počítáme s hodnotou nula a ta není > 0
 updateCounter(value);
